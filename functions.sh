8e9a81d235558766fe0f5b2a167933f506e3f6bc
Valentin Panchenko <senior.konung@gmail.com>
2025-01-16 22:35:09 +0300

    Added a check and example to the `ohcert-gen` function

--- functions.sh
+++ functions.sh
@@ -28,8 +28,12 @@ function ruclip {
     echo $1 | ru | kitten clipboard
 }
 
-# Example: ohcert-gen localhost DNS:localhost IP:192.168.0.10
 function ohcert-gen {
+    if [[ $# < 2 ]]; then
+        echo "Example: ohcert-gen localhost DNS:localhost IP:192.168.0.10"
+        return 1
+    fi
+
     local certs_dir=~/certs
     local certs_register_dir=/usr/local/share/ca-certificates
     local alt_names=$(
